4da8b9c4a100b66657badb5b322fa3abbce84da9
said.atoui@esprit.tn <said.atoui@esprit.tn>
2024-09-29 22:29:05 +0100

    add edit and create assoaction

---
+++ app/Models/Association.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Association extends Model
+{
+    use HasFactory;
+
+    protected $table = 'associations'; // Cette ligne est optionnelle si le nom de la table est déjà au pluriel.
+
+    protected $fillable = [
+        'nom', 
+    ];
+
+    /**
+     * Get the demandes for the association.
+     */
+    public function demandes()
+    {
+        return $this->hasMany(Demande::class); // Assurez-vous que le modèle Demande existe et est correctement défini
+    }
+
+  
+}
+
