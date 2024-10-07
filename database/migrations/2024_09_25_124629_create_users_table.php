501369f588c77c7f12300f70f9cc7ba838700ee7
Telgou <119911609+Telgou@users.noreply.github.com>
2024-10-07 16:45:29 +0100

    ui update

--- database/migrations/2024_09_25_124629_create_users_table.php
+++ database/migrations/2024_09_25_124629_create_users_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->bigInteger('no_hp');
-            $table->date('tanggal_lahir');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
