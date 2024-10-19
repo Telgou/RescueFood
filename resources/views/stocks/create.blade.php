bb83a44b2161b1fbf96b8d76e93468f3c30520b9
Nidhal Nar <124619795+NidhalNar@users.noreply.github.com>
2024-10-19 14:39:42 +0100

    create

--- resources/views/stocks/create.blade.php
+++ resources/views/stocks/create.blade.php
@@ -1,124 +1,95 @@
-<!DOCTYPE html>
-<html lang="en" data-bs-theme="dark">
+@extends('layouts.app-admin')
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title> Admin Dashboard</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" />
-    <script src="https://kit.fontawesome.com/ae360af17e.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ asset('admin_assets2/css/style.css') }}" />
-</head>
+@section('content')
+<div class="mb-3">
+    <h4>Admin Dashboard</h4>
+</div>
+<div class="row">
+    <div class="col-12 col-md-6 d-flex">
+        <h2>Welcome Back, {{ Auth::user()->name }}</h2>
+    </div>
 
-<body>
-    <div class="wrapper">
-        <aside id="sidebar" class="js-sidebar">
-            <!-- Content For Sidebar -->
-            <div class="h-100">
-                <div class="sidebar-logo">
-                    <a href="#">Mitra Dashboard</a>
-                </div>
-                <ul class="sidebar-nav">
-                    <li class="sidebar-header">Navigation Sidebar</li>
-                    <li class="sidebar-item">
-                        <a href="{{url('mitra/dashboard')}}" class="sidebar-link">
-                            <i class="fa-solid fa-list pe-2"></i>
-                            Dashboard
-                        </a>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="{{url('menus')}}" class="sidebar-link active">
-                            <i class="fa-solid fa-hamburger pe-2"></i>
-                            Menu
-                        </a>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="{{url('stocks')}}" class="sidebar-link">
-                            <i class="fa-solid fa-archive pe-2"></i>
-                            Stock
-                        </a>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="{{url('orders')}}" class="sidebar-link">
-                            <i class="fa-solid fa-comment-dollar pe-2"></i>
-                            Pemesanan
-                        </a>
-                    </li>
-                </ul>
+    <div class="container">
+        <!-- Card Start -->
+        <div class="card my-4">
+            <div class="card-header">
+                <h1>Create New Stock</h1>
             </div>
-        </aside>
-        <div class="main">
-            <nav class="navbar navbar-expand px-3 border-bottom">
-                <button class="btn" id="sidebar-toggle" type="button">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="navbar-collapse navbar">
-                    <ul class="navbar-nav">
-                        <li class="nav-item dropdown">
-                            <a href="#" data-bs-toggle="dropdown" class="nav-icon pe-md-0">
-                                <img src="{{ asset('images/profile.jpg') }}" class="avatar img-fluid rounded" alt="" />
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <a href="{{ route('logout') }}" class="dropdown-item">Logout</a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <main class="content px-3 py-2">
-                <div class="container mt-5 card card-body">
-                    <h2>Create Stock</h2>
-                    @if ($errors->any())
+            <div class="card-body">
+                @if ($errors->any())
                     <div class="alert alert-danger">
-                        <strong>Error!</strong> <br>
                         <ul>
                             @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
+                                <li>{{ $error }}</li>
                             @endforeach
                         </ul>
                     </div>
-                    @endif
+                @endif
 
-                    <form action="{{ route('stocks.store') }}" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="menu_id" class="form-label">Select Menu:</label>
-                            <select class="form-control" id="menu_id" name="menu_id" required>
-                                @foreach($menus as $menu)
-                                <option value="{{ $menu->id }}">{{ $menu->nama_menu }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="quantity" class="form-label">Quantity:</label>
-                            <input type="number" class="form-control" id="quantity" name="quantity" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
-            </main>
-            <a href="#" class="theme-toggle">
-                <i class="fa-regular fa-moon"></i>
-                <i class="fa-regular fa-sun"></i>
-            </a>
-            <footer class="footer">
-                <div class="container-fluid">
-                    <div class="row text-muted">
-                        <div class="col-6 text-start">
-                            <p class="mb-0">
-                                <a href="#" class="text-muted">
-                                    <strong>Coffside</strong>
-                                </a>
-                            </p>
-                        </div>
+                <form action="{{ route('stockss.store') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="food" class="form-label">Food</label>
+                        <input type="text" class="form-control @error('food') is-invalid @enderror" id="food" name="food" value="{{ old('food') }}" >
+                        @error('food')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
-                </div>
-            </footer>
+                
+                    <div class="mb-3">
+                        <label for="expiration_date" class="form-label">Expiration Date</label>
+                        <input type="date" class="form-control @error('expiration_date') is-invalid @enderror" id="expiration_date" name="expiration_date" value="{{ old('expiration_date') }}" >
+                        @error('expiration_date')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                
+                    <div class="mb-3">
+                        <label for="quantity" class="form-label">Quantity</label>
+                        <input type="number" class="form-control @error('quantity') is-invalid @enderror" id="quantity" name="quantity" value="{{ old('quantity') }}" >
+                        @error('quantity')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                
+                    <div class="mb-3">
+                        <label for="location" class="form-label">Location</label>
+                        <input type="text" class="form-control @error('location') is-invalid @enderror" id="location" name="location" value="{{ old('location') }}" >
+                        @error('location')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                
+                    <div class="mb-3">
+                        <label for="category_id" class="form-label">Category</label>
+                        <select class="form-control @error('category_id') is-invalid @enderror" id="category_id" name="category_id" >
+                            <option value="">Select a Category</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>{{ $category->type }}</option>
+                            @endforeach
+                        </select>
+                        @error('category_id')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                
+                    <button type="submit" class="btn btn-primary">Create Stock</button>
+                </form>
+                
+            </div>
         </div>
+        <!-- Card End -->
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('admin_assets2/js/script.js')}}"></script>
-</body>
+</div>
 
-</html>
+@endsection
